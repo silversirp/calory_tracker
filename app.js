99961468bf98911820f43b47c625d1a9239e559c
Silver Sirp <silver.sirp@khk.ee>
2022-01-13 13:44:32 +0200

    Populated data structure items to list

--- app.js
+++ app.js
@@ -21,6 +21,9 @@ const ItemCtrl = (function(){
     }
 
     return {
+        getItems: function(){
+            return data.items
+        },
         logData: function(){
             return data
         }
@@ -29,7 +32,25 @@ const ItemCtrl = (function(){
 
 //UI Controller
 const UICtrl = (function(){
-    console.log('UICtrl')
+    return{
+        populateItemList: function(items){
+            //create html content
+            let html = '';
+
+            //parse data and create list items html
+            items.forEach(function (item) {
+                html += `<li class="collection-item" id="item-${item.id}">
+        <strong>${item.name}: </strong> <em>${item.calories} Calories</em>
+        <a href="#" class="secondary-content">
+            <i class="fas fa-pencil-alt"></i>
+        </a>
+        </li>`
+            });
+
+            //insert list items
+            document.querySelector("#item-list").innerHTML = html;
+        }
+    }
 })();
 
 //App Controller
@@ -37,6 +58,10 @@ const App = (function(ItemCtrl, UICtrl){
     return {
         init: function (){
             console.log('Initializing App')
+            //fetch items from data structure
+            const items = ItemCtrl.getItems()
+            //populate items list
+            UICtrl.populateItemList(items)
         }
     }
 })(ItemCtrl, UICtrl);
